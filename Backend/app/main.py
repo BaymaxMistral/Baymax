19412294e05b4acaddf8218dd746afae56cae092
BaymaxMistral <pavan.y@valuehealthsol.com>
2024-06-28 18:45:39 +0530

    Update main.py
    added html filter

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,11 +2,19 @@ from typing import List
 import uvicorn
 from fastapi import BackgroundTasks, FastAPI, HTTPException, Security, status
 from fastapi.middleware.cors import CORSMiddleware
-from schemas import User_query_model            # model
-from model_inference import Process_user_query  # api
+
+
+# model
+from user_query_model import User_query_model
+
+# api
+from inference_api import Process_user_query
 
 app = FastAPI(title="mistral-hackathon-app")
+
 origins = ["*"]
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,17 +25,20 @@ app.add_middleware(
 )
 
 
-# routes
+
+# \\ routes
 @app.post("/get_response")
 async def get_response(params: User_query_model):
     # \\ call the model here
     obj1 = Process_user_query()
     response = obj1.start_query_process(user_query=params.user_query)
+    response = obj1.parse_to_html(response["response"])
     return response
 
 
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
 
 
 
